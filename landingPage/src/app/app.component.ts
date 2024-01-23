186a8e45cc4463e31693b6f19cfa4dc36b540822
Hassan <hassan.mansoor96@gmail.com>
2024-01-24 00:40:35 +0200

    main components added with dummy data

--- landingPage/src/app/app.component.ts
+++ landingPage/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import {HeaderComponent} from "./header/header.component";
+import {FirstSectionComponent} from "./first-section/first-section.component";
+import {SecondSectionComponent} from "./second-section/second-section.component";
+import {ThirdSectionComponent} from "./third-section/third-section.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavBarComponent, HeaderComponent, FirstSectionComponent, SecondSectionComponent,
+              ThirdSectionComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'landingPage';
